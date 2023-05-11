b695f18360502d846392571e5077f5f6bca9db16
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2023-05-11 15:33:07 +0600

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,7 @@ import {
   footer_title,
   footer_divider,
   copyright_text,
+  footer_menu,
 } from "./Footer.module.css";
 
 function Footer() {
@@ -14,6 +15,11 @@ function Footer() {
       <div className="container">
         <div className={footer_content}>
           <h2 className={footer_title}>MIMS Inventory System</h2>
+          <ul className={footer_menu}>
+            {["About Us", "Contact", "MIMS"].map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
           <div className={footer_divider} />
 
           <p className={copyright_text}>
