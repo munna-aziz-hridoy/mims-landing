9f1de10d7b4c8349c1f643ab8f9df079dc33531e
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2023-05-24 18:13:48 +0600

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { BsArrowRightShort } from "react-icons/bs";
+
 import {
   footer,
   footer_content,
@@ -7,6 +9,13 @@ import {
   footer_divider,
   copyright_text,
   footer_menu,
+  footer_menus,
+  footer_logo,
+  footer_subscribe,
+  footer_more,
+  subscribe_text,
+  footer_address,
+  footer_menu_title,
 } from "./Footer.module.css";
 
 function Footer() {
@@ -14,12 +23,44 @@ function Footer() {
     <div className={footer}>
       <div className="container">
         <div className={footer_content}>
-          <h2 className={footer_title}>MIMS Inventory System</h2>
-          <ul className={footer_menu}>
-            {["About Us", "Contact", "MIMS"].map((item) => (
-              <li>{item}</li>
+          <div className={footer_menus}>
+            <div>
+              <h2 className={footer_title}>MIMS Inventory System</h2>
+              <p className={footer_address}>mims@example.com</p>
+              <p className={footer_address}>
+                Plot 11/A, Road 90, Block NE-L, <br />
+                Dhaka 1212
+              </p>
+            </div>
+            <ul className={footer_menu}>
+              <h2 className={footer_menu_title}>Column 1</h2>
+              {["About Us", "Contact", "MIMS"].map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+            {[1, 2].map((item) => (
+              <ul className={footer_menu}>
+                <h2 className={footer_menu_title}>Column {item + 1}</h2>
+                {[1, 2, 3, 4, 5].map((item) => (
+                  <li>Demo Menu {item}</li>
+                ))}
+              </ul>
             ))}
-          </ul>
+            <div>
+              <h2 className={footer_logo}>Mirailit</h2>
+              <p className={footer_more}>
+                More about us <BsArrowRightShort fontSize={22} />{" "}
+              </p>
+              <p className={subscribe_text}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Architecto distinctio officia.
+              </p>
+              <div className={footer_subscribe}>
+                <input placeholder="Email" />
+                <button>Subscribe</button>
+              </div>
+            </div>
+          </div>
           <div className={footer_divider} />
 
           <p className={copyright_text}>
